3c73abe60179d4700e7d6ee29506e1e26cba5829
wvanvian <146589485+wvanvian@users.noreply.github.com>
2023-11-04 19:07:17 +0000

    almost there

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,60 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+
+  def insert
+    @movie = Movie.new
+    @movie.title = params.fetch("query_title")
+    @movie.year = params.fetch("query_year")
+    @movie.duration = params.fetch("query_duration")
+    @movie.description = params.fetch("query_description")
+    @movie.image = params.fetch("query_image")
+    @movie.director_id = params.fetch("query_director_id")
+
+    if @movie.valid?
+      @movie.save
+      redirect_to("/movies", { :notice => "Movie created successfully." })
+    else
+      redirect_to("/movies", { :notice => "Movie failed to create successfully." })
+    end
+  end
+
+
+
+
+  def update
+    the_id = params.fetch("path_id")
+    @movie = Movie.where({ :id => the_id }).at(0)
+    @movie.title = params.fetch("query_title")
+    @movie.year = params.fetch("query_year")
+    @movie.duration = params.fetch("query_duration")
+    @movie.description = params.fetch("query_description")
+    @movie.image = params.fetch("query_image")
+    @movie.director_id = params.fetch("query_director_id")
+
+
+
+    if @movie.valid?
+      @movie.save
+      redirect_to("/movies/#{the_id}", { :notice => "Movie updated successfully."} )
+    else
+      redirect_to("/movies/#{the_id}", { :alert => "Movie failed to update successfully." })
+    end
+  end
+
+
+
+
+
+
+  def delete
+    the_id = params.fetch("path_id")
+
+    @movie = Movie.where({ :id => the_id }).at(0)
+    @movie.destroy
+
+    redirect_to("/movies", { :notice => "Movie deleted successfully."} )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,11 @@ Rails.application.routes.draw do
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
+  post("/insert_movie", { :controller => "movies", :action => "insert" })
+  post("/modify_movie/:path_id", { :controller => "movies", :action => "update" })
+  get("/delete_movie/:path_id", { :controller => "movies", :action => "delete" })
+
+
   
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
